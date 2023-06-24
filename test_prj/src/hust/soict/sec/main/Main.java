0ab110440d61a7ed5ae070d5e6575e6880eda91e
ngocvu2405 <ngoc.vh.hust2003@gmail.com>
2023-06-25 02:05:05 +0700

    airtable error

--- test_prj/src/hust/soict/sec/main/Main.java
+++ test_prj/src/hust/soict/sec/main/Main.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.util.*;
 import java.io.File;
 
+import com.google.gson.JsonObject;
+import com.google.gson.*;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restfb.DefaultFacebookClient;
@@ -11,6 +14,7 @@ import com.restfb.Version;
 import com.restfb.types.User;
 
 import hust.soict.sec.objectFb.user.*;
+import hust.soict.sec.airtable.Record;
 
 public class Main{
 	static ObjectMapper mapper = new ObjectMapper();
@@ -34,9 +38,17 @@ public class Main{
         
         
         //Transfer USER to JSON file
-		String jsonUser = mapper.writeValueAsString(fullUser);
+        String json1 = mapper.writeValueAsString(userClient);
 		File userFile = new File("E:\\Nam_2\\2022.2\\prj1\\test_prj\\JSONData\\user.json");
 		mapper.writeValue(userFile, fullUser);
+		Gson gson = new Gson();		
+
+		JsonObject resultUser1 = gson.fromJson(json1, JsonObject.class);
+		System.out.println(resultUser1);
+
+		Record.createRecord(resultUser1, "tblexw8RrU1S7drHh", "appfpkYiYDZtMWJhA", "patDHXbaPvYn30swA.1e8a7fabfa00ccb9e2687143b1b79f46bd864fe86d10256f1ca44a4125046e45");
+//		Record.createRecord(resultUser, "appfpkYiYDZtMWJhA", "USER", "patDHXbaPvYn30swA.1e8a7fabfa00ccb9e2687143b1b79f46bd864fe86d10256f1ca44a4125046e45");
+		
 	}
 
 }
