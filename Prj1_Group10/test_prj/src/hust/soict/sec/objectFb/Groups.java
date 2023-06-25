cc936fb2d29693b1b02bb4a2ac6df6006f618163
Nguyen Duy An <87957243+kbitgipt@users.noreply.github.com>
2023-06-25 20:30:09 +0700

    Update Groups.java

--- Prj1_Group10/test_prj/src/hust/soict/sec/objectFb/Groups.java
+++ Prj1_Group10/test_prj/src/hust/soict/sec/objectFb/Groups.java
@@ -1,22 +1,20 @@
 package hust.soict.sec.objectFb;
 
-import java.util.Date;
-
 public class Groups {
 	private String Id;
     private String name;
-    private Date updated_time;
+    private String description;
     
     public Groups() {};
     
-    public Groups(String Id, String name, Date updated_time) {
+    public Groups(String Id, String name, String description) {
         this.name = name;
-        this.updated_time = updated_time;
+        this.description = description;
         this.Id = Id;
     }
 
-	public Date getupdated_time() {
-		return updated_time;
+	public String description() {
+		return description;
 	}
 
 	public String getName() {
