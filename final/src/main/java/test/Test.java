79e5e4ce7451096817c6dec498e9c364d16e3ea5
Nguyen Duy An <87957243+kbitgipt@users.noreply.github.com>
2023-07-06 13:27:49 +0700

    Add files via upload

--- final/src/main/java/test/Test.java
+++ final/src/main/java/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 import facebook.*;
 import facebook.user.Group;
+import facebook.user.GroupAdmin;
 import facebook.user.GroupFeed;
 import facebook.user.User;
 
@@ -10,8 +11,10 @@ public class Test {
 
 	public static void main(String[] args) {
 //		User userFb = new User("me?fields", "id%2Cname");
-		GroupFeed groupFb = new GroupFeed("me?fields=groups%7Bfeed%7Bid%2Cmessage%2Ccreated_time%7D%2Cmember_count%2Cid%2Cname%2Cdescription%2Cadministrator%7D&access_token=");
-        String order = groupFb.order;
+		GroupAdmin groupFb = new GroupAdmin("me?fields=groups%7Bid%2Cname%2Cdescription%2Cadministrator%2Cmember_count%2Cmember_request_count%7D&access_token=");
+       
+        GroupFeed groupfeed = new GroupFeed("me?fields=groups%7Bfeed%7Bid%2Cmessage%2Ccreated_time%7D%7D&access_token=");
+         String order = groupfeed.order;
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the Facebook access token: ");
         String accessToken = scanner.nextLine();
