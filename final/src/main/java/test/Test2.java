15fb1d69f4b3f4916951aaf0db0bc121c38e133e
Ngoc Vu <89976500+ngocvu2405@users.noreply.github.com>
2023-07-08 01:50:32 +0700

    Add files via upload

--- final/src/main/java/test/Test2.java
+++ final/src/main/java/test/Test2.java
@@ -8,12 +8,12 @@ public class Test2 {
 	public static void main(String[] args) {
 		RecordUser user = new RecordUser();
 		// TODO Auto-generated method stub
-		String data = user.reformatData();
-		try {
-			user.POSTRequest("appfpkYiYDZtMWJhA", "tblexw8RrU1S7drHh", "patJOGkmzGUONSJVC.1e139f03d8fc3789fa64c266896a4a32fd875c90d0c83e895e28e49a44ed89b7", data);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		user.reformatData();
+		
+		RecordGroup group = new RecordGroup();
+		
+		group.reformatData();
+
 	}
 
 }
