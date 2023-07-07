ec13aba6ce288a5d99aa48ddeea362b92b327b74
Nguyen Duy An <87957243+kbitgipt@users.noreply.github.com>
2023-07-08 02:05:13 +0700

    Add files via upload

--- final/src/main/java/airtable/RecordGroup.java
+++ final/src/main/java/airtable/RecordGroup.java
@@ -13,10 +13,8 @@ public class RecordGroup extends PostRequest {
 
 	@Override
 	public String reformatData() {
-		Group userGr = new Group("me?fields=groups%7Bid%2Cname%2Cdescription%2Cadministrator%7D&access_token=");
-		String order = userGr.order;
-
-        
+		Group userGr = new Group("me?fields=groups%7Bid%2Cname%2Cdescription%2Cadministrator%2Cmember_count%7D&access_token=");
+        String order = userGr.order;
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the Facebook access token: ");
         String accessToken = scanner.nextLine();
@@ -24,31 +22,33 @@ public class RecordGroup extends PostRequest {
 		try {
 			String res = GetData.getData(accessToken, order);
 			System.out.println(res);
-        	JSONObject jsonObject = new JSONObject(res);
+
+			JSONObject jsonObject = new JSONObject(res);
 	        JSONArray jsonArray = jsonObject.getJSONObject("groups").getJSONArray("data");
 
-		        for (int i = 0; i < jsonArray.length(); i++) {
+	        for (int i = 0; i < jsonArray.length(); i++) {
 
-			        JSONArray recordsArray = new JSONArray();
-		            JSONObject fieldsObject = new JSONObject();
-		            JSONObject dataObject = jsonArray.getJSONObject(i);
-		            
-		            fieldsObject.put("description", dataObject.isNull("description") ? null : dataObject.getString("description"));
-		            fieldsObject.put("id", dataObject.getString("id"));
-		            fieldsObject.put("name", dataObject.getString("name"));
-		            fieldsObject.put("administrator", dataObject.getBoolean("administrator"));
-
-		            JSONObject recordObject = new JSONObject();
-		            recordObject.put("fields", fieldsObject);
-		            recordsArray.put(recordObject);
-		            JSONObject outputObject = new JSONObject();
-			        outputObject.put("records", recordsArray);
-			        String resData = outputObject.toString();
-			        System.out.println(resData);
-			        POSTRequest("appfpkYiYDZtMWJhA", "tblhmlceroOgnh6Ed", "patJOGkmzGUONSJVC.1e139f03d8fc3789fa64c266896a4a32fd875c90d0c83e895e28e49a44ed89b7", resData);
-		        }
-	        
-			return null;
+		        JSONArray recordsArray = new JSONArray();
+	            JSONObject fieldsObject = new JSONObject();
+	            JSONObject dataObject = jsonArray.getJSONObject(i);
+	            
+	            fieldsObject.put("description", dataObject.isNull("description") ? null : dataObject.getString("description"));
+	            fieldsObject.put("id", dataObject.getString("id"));
+	            fieldsObject.put("name", dataObject.getString("name"));
+	            fieldsObject.put("administrator", dataObject.getBoolean("administrator"));
+	            fieldsObject.put("member_count", dataObject.isNull("member_count") ? null : dataObject.getInt("member_count"));
+	            
+	            JSONObject recordObject = new JSONObject();
+	            recordObject.put("fields", fieldsObject);
+	            recordsArray.put(recordObject);
+	            JSONObject outputObject = new JSONObject();
+		        outputObject.put("records", recordsArray);
+		        String resData = outputObject.toString();
+		        System.out.println(resData);
+		        POSTRequest("appfpkYiYDZtMWJhA", "tbl4FG2B4nb6RwtWI", "patJOGkmzGUONSJVC.1e139f03d8fc3789fa64c266896a4a32fd875c90d0c83e895e28e49a44ed89b7", resData);
+	        }
+        
+		return null;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return "error: " +e.getMessage();
