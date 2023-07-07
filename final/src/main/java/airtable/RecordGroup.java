0e959a90a1c9a1f88edb213d67985f9214b5bb48
Ngoc Vu <89976500+ngocvu2405@users.noreply.github.com>
2023-07-08 01:49:47 +0700

    Add files via upload

--- final/src/main/java/airtable/RecordGroup.java
+++ final/src/main/java/airtable/RecordGroup.java
@@ -7,22 +7,22 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import facebook.GetData;
-import facebook.group.*;
+import facebook.user.*;
 
 public class RecordGroup extends PostRequest {
 
 	@Override
 	public String reformatData() {
-		Group userGr = new Group("me?fields", "groups%7Bid%2Cname%2Cdescription%2Cadministrator%7D");
-		String objectId = userGr.objectId;
-        String listAttribute = userGr.listAttribute;
+		Group userGr = new Group("me?fields=groups%7Bid%2Cname%2Cdescription%2Cadministrator%7D&access_token=");
+		String order = userGr.order;
+
         
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the Facebook access token: ");
         String accessToken = scanner.nextLine();
         Group.setAccessToken(accessToken);
 		try {
-			String res = GetData.getData(objectId,listAttribute,accessToken);
+			String res = GetData.getData(accessToken, order);
 			System.out.println(res);
         	JSONObject jsonObject = new JSONObject(res);
 	        JSONArray jsonArray = jsonObject.getJSONObject("groups").getJSONArray("data");
