96f7dffc0c10baf1834f81094acafb9ff77bee25
Ngoc Vu <89976500+ngocvu2405@users.noreply.github.com>
2023-07-20 03:01:14 +0700

    Add files via upload

--- final/src/main/java/facebook/user/User.java
+++ final/src/main/java/facebook/user/User.java
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+package facebook.user;
+
+import facebook.GetData;
+
+public class User extends GetData{
+
+    public String order = new String("me?fields=id%2Cname&access_token=");
+		
+	public User(String order) {
+		super(order);
+		this.order = order;
+	}
+
+}
+=======
 package facebook.user;
 
 import java.io.IOException;
@@ -14,3 +30,4 @@ public class User extends GetData{
 	}
 
 }
+>>>>>>> 03b99985bb22b10552bf9ce159b1fdfbce4e1a3c
